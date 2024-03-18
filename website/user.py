318b35eab1b7dada0fb67dc4b11094027156cbbd
Francisco Valdez <franciscovaldez86@gmail.com>
2024-03-18 16:52:09 -0500

    Reorganizacion de archivos y completo base de gestor para admin

--- website/user.py
+++ website/user.py
@@ -1,9 +1,10 @@
 from flask import Blueprint, render_template, render_template
-from flask_login import  login_required
+from flask_login import  login_required, current_user
 
 user = Blueprint('user', __name__)
 
 @user.route('/profile')
 @login_required
 def profile():
-    return render_template("base.html")
+    print(current_user.is_authenticated)
+    return render_template("admin.html")
